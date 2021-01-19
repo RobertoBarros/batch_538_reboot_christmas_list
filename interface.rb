96e8867f2a603ec17a44af2f9d74f8d817dc53ff
Roberto Barros <roberto.f.barros@gmail.com>
2021-01-19 12:21:12 -0300

    list action working

--- interface.rb
+++ interface.rb
@@ -1,11 +1,28 @@
-#saudacao
-puts " Welcome to your Christmas list"
-#mostrar o menulist/ add/ delete/ mark)
-action = nil
+# saudacao
+puts "---- Welcome to your Christmas list -------"
 
+def list(gifts)
+  puts 'No items in list' if gifts.count.zero?
+
+  gifts.each_with_index do |gift, index|
+    puts "#{index + 1} - #{gift}"
+  end
+end
+
+gifts = []
+
+action = nil
 # condicao de pergunta e acao
-while action != "quit"
-  puts "Which action [list|add|delete|quit]?"
+while action != 'quit'
+  # mostrar o menu list / add / delete / mark)
+  puts 'Which action list|add|quit?'
   action = gets.chomp
+
+  case action
+  when 'list' then list(gifts)
+  else
+    puts "Invalid action"
+  end
+
 end
 
