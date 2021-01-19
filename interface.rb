379670b141872ff95019328f4014f31a7a7025e1
Roberto Barros <roberto.f.barros@gmail.com>
2021-01-19 13:46:58 -0300

    add items working

--- interface.rb
+++ interface.rb
@@ -11,18 +11,26 @@ end
 
 gifts = []
 
-action = nil
-# condicao de pergunta e acao
-while action != 'quit'
+def add(gifts)
+  puts 'Which item do you want to add?'
+  user_answer = gets.chomp
+  gifts << user_answer
+end
+
+loop do
   # mostrar o menu list / add / delete / mark)
   puts 'Which action list|add|quit?'
   action = gets.chomp
 
   case action
   when 'list' then list(gifts)
+  when 'add' then add(gifts)
+  when 'quit' then break
   else
-    puts "Invalid action"
+    puts 'Invalid action'
   end
-
 end
+
+puts "Goodbye..."
+
 
