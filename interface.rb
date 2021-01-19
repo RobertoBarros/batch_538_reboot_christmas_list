81798a626b9318cd71e6539ca9bfa150188a301a
Roberto Barros <roberto.f.barros@gmail.com>
2021-01-19 14:32:59 -0300

    CSV load and save

--- interface.rb
+++ interface.rb
@@ -1,5 +1,25 @@
+require 'csv'
 # saudacao
 puts "---- Welcome to your Christmas list -------"
+
+# Carrega os TODOS salvos no arquivo csv_filepath (todos.csv)
+def load_csv(csv_filepath)
+  gifts = []
+  CSV.foreach(csv_filepath) do |row|
+    gifts << row[0] # Nosso csv só tem uma coluna
+  end
+  return gifts
+end
+
+# Salva o CSV no arquivo csv_filepath (todos.csv) sempre apagando e criando novamente
+def save_csv(csv_filepath, gifts)
+  CSV.open(csv_filepath, 'wb') do |csv|
+    gifts.each do |gift| # Itera por cada todo do array com todos os TODOs
+      csv << [gift] # Precisa colocar em um array porque o CSV é um array de arrays
+    end
+  end
+end
+
 
 def list(gifts)
   puts 'No items in list' if gifts.count.zero?
@@ -9,12 +29,11 @@ def list(gifts)
   end
 end
 
-gifts = []
-
 def add(gifts)
   puts 'Which item do you want to add?'
   user_answer = gets.chomp
   gifts << user_answer
+  save_csv('gifts.csv', gifts)
 end
 
 def delete(gifts)
@@ -26,8 +45,14 @@ def delete(gifts)
     puts 'Invalid Number'
   else
     gifts.delete_at(index)
+    save_csv('gifts.csv', gifts)
   end
 end
+
+
+
+gifts = load_csv('gifts.csv')
+
 
 loop do
   # mostrar o menu list / add / delete / mark)
